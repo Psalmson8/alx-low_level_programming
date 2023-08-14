d7bd9dad90a5e0e4348848e6da80bed136f9296e
Samson Ayeni <ayenisamson8@gmail.com>
2023-08-14 08:19:31 -0700

    4put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - This Programming is like building a multilingual puzzle
+ *
+ * Return: this returns 0
+ */
+
+int main(void)
+{
+     puts("programming is like building a multilingual puzzle.");
+     return (0);
+}     
